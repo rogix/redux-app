7cff68a1029cfdcdbdacc7c2c8ece9d90ea127be
Rogerio Pereira <rogix@Mac-mini-de-Rogerio.local>
2019-07-01 17:18:55 -0300

    component video

---
+++ src/components/Video/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Video extends Component {
+  render() {
+    return (
+      <div>
+        <strong>Modulo X</strong>
+        <span>Aula X</span>
+      </div>
+    );
+  }
+}
